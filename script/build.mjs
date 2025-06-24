b2fd8cc8ec3dfd15a2cf6f5fae507d9135e12a4a
redhoodsu <redhoodsu@tencent.com>
2025-06-24 12:11:32 +0800

    feat: build

---
+++ script/build.mjs
@@ -0,0 +1,25 @@
+const pkg = await fs.readJson('package.json')
+const electron = pkg.devDependencies.electron
+delete pkg.devDependencies
+pkg.devDependencies = {
+  electron,
+}
+delete pkg.scripts
+pkg.scripts = {
+  start: 'electron main/index.js',
+}
+pkg.main = 'main/index.js'
+
+await $`npm run build:main`
+await $`npm run build:preload`
+await $`npm run build:renderer`
+
+await fs.copy('build', 'dist/build')
+await fs.copy('rclone', 'dist/rclone')
+cd('dist')
+
+await fs.writeJson('package.json', pkg, {
+  spaces: 2,
+})
+
+await $`npm i --production`
---
+++ script/pack.mjs
@@ -0,0 +1,48 @@
+import builder from 'electron-builder'
+import isMac from 'licia/isMac.js'
+
+cd('dist')
+
+const pkg = await fs.readJson('package.json')
+
+let publishChannel = '${productName}-latest'
+if (isMac && process.arch !== 'arm64') {
+  publishChannel = '${productName}-latest-${arch}'
+}
+
+const config = {
+  directories: {
+    output: `../release/${pkg.version}`,
+  },
+  files: ['main', 'preload', 'renderer', 'rclone'],
+  asarUnpack: ['rclone/**/*'],
+  artifactName: '${productName}-${version}-${os}-${arch}.${ext}',
+  nsis: {
+    allowToChangeInstallationDirectory: true,
+    oneClick: false,
+  },
+  win: {
+    target: [
+      {
+        target: 'nsis',
+      },
+    ],
+  },
+  mac: {
+    electronLanguages: ['zh_CN', 'en'],
+    target: [
+      {
+        target: 'dmg',
+      },
+    ],
+  },
+  publish: {
+    provider: 'generic',
+    url: 'https://release.liriliri.io/',
+    channel: publishChannel,
+  },
+}
+
+await builder.build({
+  config,
+})
