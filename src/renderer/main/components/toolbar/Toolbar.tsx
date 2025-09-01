b47ba7298a8dbe8c6155b54b25051e5b832e688f
redhoodsu <redhoodsu@tencent.com>
2025-09-01 19:10:07 +0800

    chore: small changes

--- src/renderer/main/components/toolbar/Toolbar.tsx
+++ src/renderer/main/components/toolbar/Toolbar.tsx
@@ -19,110 +19,108 @@ export default observer(function Toolbar() {
   const { remote } = store
 
   return (
-    <>
-      <LunaToolbar>
-        <ToolbarIcon
-          icon="remote-explorer"
-          title={t('config')}
-          state={store.showConfig ? 'hover' : ''}
-          onClick={() => {
-            store.toggleConfig()
+    <LunaToolbar>
+      <ToolbarIcon
+        icon="remote-explorer"
+        title={t('config')}
+        state={store.showConfig ? 'hover' : ''}
+        onClick={() => {
+          store.toggleConfig()
+        }}
+      />
+      <ToolbarIcon
+        icon="bidirection"
+        className={className({
+          [Style.blink]: some(store.jobs, (job) => {
+            return job.status === JobStatus.Running
+          }),
+        })}
+        title={t('job')}
+        state={store.showJob ? 'hover' : ''}
+        onClick={() => {
+          store.toggleJob()
+        }}
+      />
+      <LunaToolbarSeparator />
+      <ToolbarIcon
+        icon="arrow-left"
+        title={t('back')}
+        onClick={() => remote.back()}
+        disabled={remote.historyIdx <= 0 || remote.isLoading}
+      />
+      <ToolbarIcon
+        icon="arrow-right"
+        title={t('forward')}
+        onClick={() => remote.forward()}
+        disabled={
+          remote.historyIdx >= remote.history.length - 1 || remote.isLoading
+        }
+      />
+      <ToolbarIcon
+        icon="arrow-up"
+        title={t('up')}
+        onClick={() => remote.up()}
+        disabled={!remote.remote || remote.isLoading}
+      />
+      <ToolbarIcon
+        icon="refresh"
+        title={t('refresh')}
+        onClick={() => remote.refresh()}
+        disabled={remote.isLoading}
+      />
+      <LunaToolbarHtml className={Style.pathContainer}>
+        <LunaPathBar
+          className={Style.path}
+          path={remote.customRemote}
+          onChange={async (path) => {
+            const { customRemote } = remote
+            remote.setCustomRemote(path)
+            const p = trim(normalizePath(path.replace(remote.name, '')), '/')
+            if (p === remote.remote) {
+              return
+            }
+            try {
+              await remote.go(p)
+            } catch {
+              remote.setCustomRemote(customRemote)
+            }
           }}
         />
-        <ToolbarIcon
-          icon="bidirection"
-          className={className({
-            [Style.blink]: some(store.jobs, (job) => {
-              return job.status === JobStatus.Running
-            }),
-          })}
-          title={t('job')}
-          state={store.showJob ? 'hover' : ''}
-          onClick={() => {
-            store.toggleJob()
-          }}
-        />
-        <LunaToolbarSeparator />
-        <ToolbarIcon
-          icon="arrow-left"
-          title={t('back')}
-          onClick={() => remote.back()}
-          disabled={remote.historyIdx <= 0 || remote.isLoading}
-        />
-        <ToolbarIcon
-          icon="arrow-right"
-          title={t('forward')}
-          onClick={() => remote.forward()}
-          disabled={
-            remote.historyIdx >= remote.history.length - 1 || remote.isLoading
+      </LunaToolbarHtml>
+      <LunaToolbarInput
+        keyName="filter"
+        value={store.remote.filter}
+        placeholder={t('filter')}
+        onChange={(val) => store.remote.setFilter(val)}
+      />
+      <LunaToolbarSeparator />
+      <ToolbarIcon
+        icon="grid"
+        title={t('iconView')}
+        state={store.listView ? '' : 'hover'}
+        onClick={() => {
+          if (store.listView) {
+            store.setViewMode('icon')
           }
-        />
-        <ToolbarIcon
-          icon="arrow-up"
-          title={t('up')}
-          onClick={() => remote.up()}
-          disabled={!remote.remote || remote.isLoading}
-        />
-        <ToolbarIcon
-          icon="refresh"
-          title={t('refresh')}
-          onClick={() => remote.refresh()}
-          disabled={remote.isLoading}
-        />
-        <LunaToolbarHtml className={Style.pathContainer}>
-          <LunaPathBar
-            className={Style.path}
-            path={remote.customRemote}
-            onChange={async (path) => {
-              const { customRemote } = remote
-              remote.setCustomRemote(path)
-              const p = trim(normalizePath(path.replace(remote.name, '')), '/')
-              if (p === remote.remote) {
-                return
-              }
-              try {
-                await remote.go(p)
-              } catch {
-                remote.setCustomRemote(customRemote)
-              }
-            }}
-          />
-        </LunaToolbarHtml>
-        <LunaToolbarInput
-          keyName="filter"
-          value={store.remote.filter}
-          placeholder={t('filter')}
-          onChange={(val) => store.remote.setFilter(val)}
-        />
-        <LunaToolbarSeparator />
-        <ToolbarIcon
-          icon="grid"
-          title={t('iconView')}
-          state={store.listView ? '' : 'hover'}
-          onClick={() => {
-            if (store.listView) {
-              store.setViewMode('icon')
-            }
-          }}
-        />
-        <ToolbarIcon
-          icon="list"
-          title={t('listView')}
-          state={store.listView ? 'hover' : ''}
-          onClick={() => {
-            if (!store.listView) {
-              store.setViewMode('list')
-            }
-          }}
-        />
-        <LunaToolbarSeparator />
-        <ToolbarIcon
-          icon="eye"
-          title={t('preview')}
-          state={store.showPreview ? 'hover' : ''}
-          onClick={() => store.togglePreview()}
-        />
-      </LunaToolbar>
-    </>
+        }}
+      />
+      <ToolbarIcon
+        icon="list"
+        title={t('listView')}
+        state={store.listView ? 'hover' : ''}
+        onClick={() => {
+          if (!store.listView) {
+            store.setViewMode('list')
+          }
+        }}
+      />
+      <LunaToolbarSeparator />
+      <ToolbarIcon
+        icon="eye"
+        title={t('preview')}
+        state={store.showPreview ? 'hover' : ''}
+        onClick={() => store.togglePreview()}
+      />
+    </LunaToolbar>
   )
 })
